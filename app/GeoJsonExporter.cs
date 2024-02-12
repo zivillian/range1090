bf86d47926b49846e3c1ca05e35fc79a2fca2b4c
zivillian <devel@zivillian.de>
2024-02-12 21:38:27 +0100

    remove GeometryPrecisionReducer and replace by simple logic to reduce cpu and memory usage

--- app/GeoJsonExporter.cs
+++ app/GeoJsonExporter.cs
@@ -2,7 +2,6 @@
 using NetTopologySuite.Features;
 using NetTopologySuite.Geometries;
 using NetTopologySuite.IO.Converters;
-using NetTopologySuite.Precision;
 using range1090;
 
 public class GeoJsonExporter
@@ -11,7 +10,7 @@ public class GeoJsonExporter
     private readonly Coordinate _groundZero;
     private DateTime _lastExport = DateTime.MinValue;
     private readonly GeometryFactory _factory;
-    private readonly GeometryPrecisionReducer _reducer;
+    private readonly PrecisionModel _precision;
     private readonly JsonSerializerOptions _jsonOptions;
 
     public GeoJsonExporter(double latitude, double longitude, byte precision, int interval)
@@ -19,8 +18,9 @@ public class GeoJsonExporter
         _interval -= interval;
         _groundZero = new(longitude, latitude);
         //https://xkcd.com/2170/
-        _factory = new (new PrecisionModel(Math.Pow(10, precision)), 3857);
-        _reducer = new(_factory.PrecisionModel);
+        _precision = new PrecisionModel(Math.Pow(10, precision));
+        _factory = new (_precision, 3857);
+        _precision.MakePrecise(_groundZero);
         _jsonOptions = new JsonSerializerOptions
         {
             Converters = { new GeoJsonConverterFactory(_factory) }
@@ -35,26 +35,50 @@ public class GeoJsonExporter
 
         foreach (var flightLevel in levels.OrderByDescending(x => x.FlightLevel))
         {
-            var coordinates = new List<Coordinate>(360 * 2 + 1);
+            var coordinates = new List<Coordinate>(360 + 1);
             var bearings = flightLevel.Positions.ToDictionary(x => x.Bearing);
+            var lastWasNull = false;
+            var secondToLastWasNull = false;
             for (ushort bearing = 0; bearing < 360; bearing++)
             {
                 if (!bearings.TryGetValue(bearing, out var segment))
                 {
-                    coordinates.Add(_groundZero);
+                    if (lastWasNull) continue;
+                    if (secondToLastWasNull)
+                    {
+                        //remove previous single track point
+                        coordinates.RemoveAt(coordinates.Count - 1);
+                    }
+                    else
+                    {
+                        coordinates.Add(_groundZero);
+                    }
+                    secondToLastWasNull = false;
+                    lastWasNull = true;
                 }
                 else
                 {
+                    secondToLastWasNull = lastWasNull;
+                    lastWasNull = false;
                     coordinates.Add(new Coordinate(segment.Longitude, segment.Latitude));
                 }
             }
-            coordinates.Add(coordinates[0]);
+
+            if (!coordinates[0].Equals(coordinates[^1]))
+            {
+                //close the shape
+                coordinates.Add(coordinates[0]);
+            }
+            if (coordinates.Count <= 3) continue;
+
+            //https://github.com/NetTopologySuite/NetTopologySuite.IO.GeoJSON/issues/135
+            coordinates.ForEach(_precision.MakePrecise);
             var polygon = _factory.CreatePolygon(coordinates.ToArray());
             var attributes = new AttributesTable
             {
                 { "altitude", flightLevel.FlightLevel * 100 },
             };
-            var feature = new Feature(_reducer.Reduce(polygon), attributes);
+            var feature = new Feature(polygon, attributes);
             if (!feature.Geometry.IsEmpty)
             {
                 collection.Add(feature);
