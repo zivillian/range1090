d48c8eaeb7ba959638eb81e7e6ba979a23f3c396
zivillian <devel@zivillian.de>
2024-02-15 18:11:52 +0100

    fix off by one(again)

--- app/PolarRange.cs
+++ app/PolarRange.cs
@@ -15,7 +15,7 @@ public class PolarRange(double latitude, double longitude, bool verbose)
 
     public bool Add(Coordinate location, ushort flightLevel)
     {
-        if (flightLevel >= MAX_FLIGHT_LEVEL) flightLevel = MAX_FLIGHT_LEVEL;
+        if (flightLevel >= MAX_FLIGHT_LEVEL) flightLevel = MAX_FLIGHT_LEVEL-1;
         var area = _ranges[flightLevel];
         if (area is null)
         {
